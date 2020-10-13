9c89220be96239223f088cefd1c28df9aef20e40
Baskoro Nugroho <bas.nugroho@yahoo.com>
2020-10-13 14:30:38 +0700

    Initial Commit

---
+++ MyChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyChatApp
+//
+//  Created by Baskoro Nugroho on 13/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
